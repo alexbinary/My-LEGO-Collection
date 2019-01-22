130cd41a6812e73583268405420d0bac672ad06a
Alexandre Bintz <alexandre@bintz.io>
2019-01-22 20:40:38 +0100

    👍 libs: improve doc comments in Date+elapsedTimeSince

--- Libs/Date+elapsedTimeSince.swift
+++ Libs/Date+elapsedTimeSince.swift
@@ -5,14 +5,15 @@ import Foundation
 extension Date {
     
     
-    /// Returns a string that represents the amount of time between this instance and the provided date.
+    /// Returns a string that represents the amount of time between this value and the provided date.
     ///
     /// The amount of time is expressed either in seconds or milliseconds, depending on the value.
     /// Example results: "3.4s", "56ms"
     ///
     /// - Parameter date: The date to measure the elapsed time since.
     ///
-    /// - Returns: A string that represents the amount of time between this instance and the provided date, expressed either in seconds or milliseconds.
+    /// - Returns: A string that represents the amount of time between this instance and the provided date,
+    ///            expressed either in seconds or milliseconds.
     ///
     func elapsedTimeSince(_ date: Date) -> String {
     
