1f799fdf0477c0513b2cacf8f34c722c14e9774e
Alexandre Bintz <alexandre@bintz.io>
2019-02-01 18:03:43 +0100

    wip

--- Libs/SQLite/Core/SQLite_Connection.swift
+++ Libs/SQLite/Core/SQLite_Connection.swift
@@ -90,3 +90,34 @@ extension SQLite_Connection {
         statement.run()
     }
 }
+
+
+extension SQLite_Connection {
+    
+    
+    /// Reads all rows from a table.
+    ///
+    /// - Parameter table: A description of the table to read from.
+    ///
+    /// - Returns: An array of dictionnaries that contain the value of each
+    ///            column for each row.
+    ///
+    func readAllRows(from table: SQLite_Table) -> [[SQLite_Column: SQLite_ColumnValue]] {
+        
+        let query = SQLite_SelectQuery(table: table)
+        
+        let statement = SQLite_Statement(connection: self, query: query)
+        
+        return statement.readAllRows { statement in
+            
+            var row: [SQLite_Column: SQLite_ColumnValue] = [:]
+            
+            for (index, column) in table.columns.enumerated() {
+                
+                row[column] = statement.readValue(for: column, at: index)
+            }
+            
+            return row
+        }
+    }
+}
--- Libs/SQLite/Core/SQLite_Statement.swift
+++ Libs/SQLite/Core/SQLite_Statement.swift
@@ -164,3 +164,98 @@ extension SQLite_Statement {
         }
     }
 }
+
+
+extension SQLite_Statement {
+    
+    
+    func readAllRows<ResultType>(with reader: (SQLite_Statement) -> ResultType) -> [ResultType] {
+        
+        var results: [ResultType] = []
+        
+        while true {
+            
+            let stepResult = sqlite3_step(pointer)
+            
+            guard stepResult.isOneOf([SQLITE_ROW, SQLITE_DONE]) else {
+                
+                fatalError("[DatabaseController] sqlite3_step() returned \(stepResult) for query: \(query). SQLite error: \(connection.errorMessage ?? "")")
+            }
+            
+            if stepResult == SQLITE_ROW {
+                
+                results.append(reader(self))
+                
+            } else {
+                
+                break
+            }
+        }
+        
+        return results
+    }
+}
+
+
+extension SQLite_Statement {
+    
+    
+    func readValue(for column: SQLite_Column, at index: Int) -> SQLite_ColumnValue {
+        
+        switch column.type {
+            
+        case .bool:
+            
+            return readBool(at: index)
+            
+        case .char:
+            
+            if column.nullable {
+                
+                return readOptionalString(at: index)
+                
+            } else {
+                
+                return readString(at: index)
+            }
+        }
+    }
+}
+
+
+extension SQLite_Statement {
+    
+    
+    func readBool(at index: Int) -> Bool {
+        
+        let raw = sqlite3_column_int(pointer, Int32(index))
+        
+        return raw != 0
+    }
+    
+    
+    func readOptionalString(at index: Int) -> String? {
+        
+        if let raw = sqlite3_column_text(pointer, Int32(index)) {
+            
+            return String(cString: raw)
+            
+        } else {
+            
+            return nil
+        }
+    }
+    
+    
+    func readString(at index: Int) -> String {
+        
+        let value = readOptionalString(at: index)
+        
+        guard value != nil else {
+            
+            fatalError("[DatabaseController] Found NULL while expecting non null string value at index: \(index) for results of query: \(query)")
+        }
+        
+        return value!
+    }
+}
