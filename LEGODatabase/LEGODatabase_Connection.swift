d42cc38c56a7bee71108bf5dd099c173dd5b2b92
Alexandre Bintz <alexandre@bintz.io>
2019-02-01 21:01:38 +0100

    wip

--- LEGODatabase/LEGODatabase_Connection.swift
+++ LEGODatabase/LEGODatabase_Connection.swift
@@ -6,3 +6,66 @@ import Foundation
 class LEGODatabase_Connection: SQLite_Connection {
     
 }
+
+
+extension LEGODatabase_Connection {
+    
+    
+    func createColorsTable() {
+        
+        create(table: LEGODatabase.schema.colorsTable)
+    }
+    
+    
+    func createPartsTable() {
+        
+        create(table: LEGODatabase.schema.partsTable)
+    }
+}
+
+
+extension LEGODatabase_Connection {
+    
+    
+    func prepareColorInsertStatement() -> ColorInsert_Statement {
+        
+        return ColorInsert_Statement(connection: self)
+    }
+    
+    
+    func preparePartInsertStatement() -> PartInsertStatement {
+        
+        return PartInsertStatement(connection: self)
+    }
+}
+
+
+extension LEGODatabase_Connection {
+    
+    
+    func readAllColors() -> [LEGODatabase_Schema.ColorsTable.Row] {
+        
+        return readAllRows(from: LEGODatabase.schema.colorsTable).map { values in
+            
+            return LEGODatabase_Schema.ColorsTable.Row(
+                
+                name: values[LEGODatabase.schema.colorsTable.nameColumn] as! String,
+                rgb: values[LEGODatabase.schema.colorsTable.rgbColumn] as! String,
+                transparent: values[LEGODatabase.schema.colorsTable.transparentColumn] as! Bool
+            )
+        }
+    }
+    
+    
+    func readAllParts() -> [LEGODatabase_Schema.PartsTable.Row] {
+        
+        return readAllRows(from: LEGODatabase.schema.partsTable).map { values in
+            
+            return LEGODatabase_Schema.PartsTable.Row(
+                
+                name: values[LEGODatabase.schema.partsTable.nameColumn] as! String,
+                imageURL: values[LEGODatabase.schema.partsTable.imageURLColumn] as! String?
+            )
+        }
+    }
+}
