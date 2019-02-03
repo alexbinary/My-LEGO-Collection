9326175f0c763d265405f2303064187dbd9e7963
Alexandre Bintz <alexandre@bintz.io>
2019-02-03 11:35:04 +0100

    wip

---
+++ Libs/SQLite/Core/SQLite_SelectStatement.swift
@@ -0,0 +1,62 @@
+
+import Foundation
+
+
+
+/// A statement that inserts data into a table.
+///
+/// This class provides convenience methods that facilitate the execution of
+/// "INSERT INTO" statements.
+///
+class SQLite_SelectStatement: SQLite_Statement {
+    
+    
+    /// The table the statement inserts data into.
+    ///
+    private let table: SQLite_Table
+    
+    
+    /// The insert query that was used to compile the statement.
+    ///
+    private let insertQuery: SQLite_InsertQuery
+    
+    
+    /// Creates a new statement for a given table.
+    ///
+    /// - Parameter table: The table the statement should insert data into.
+    /// - Parameter connection: The connection to use to compile the query.
+    ///
+    init(for table: SQLite_Table, connection: SQLite_Connection) {
+        
+        self.table = table
+        self.insertQuery = SQLite_InsertQuery(insertingInto: table)
+        
+        super.init(connection: connection, query: insertQuery)
+    }
+}
+
+
+extension SQLite_SelectStatement {
+    
+    
+    /// Inserts values in the table.
+    ///
+    /// - Parameter columnValues: The value to insert in each column.
+    ///
+    func insert(_ columnValues: [SQLite_Column: SQLite_QueryParameterValue]) {
+        
+        var parameterValues: [SQLite_QueryParameter: SQLite_QueryParameterValue] = [:]
+        
+        for (column, parameter) in insertQuery.parameters {
+        
+            guard let value = columnValues[column] else {
+                
+                fatalError("[SQLite_InsertStatement] Missing value for column: \(column). Trying to insert into table: \(table.name), values: \(columnValues)")
+            }
+            
+            parameterValues[parameter] = value
+        }
+        
+        _ = runThroughCompletion(with: parameterValues)
+    }
+}
