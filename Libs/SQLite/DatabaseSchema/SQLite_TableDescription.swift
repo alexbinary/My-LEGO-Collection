efa02ad7c70695e3e3e5bda1923fc33b68e2c471
Alexandre Bintz <alexandre@bintz.io>
2019-02-05 20:17:14 +0100

    wip

--- Libs/SQLite/DatabaseSchema/SQLite_TableDescription.swift
+++ Libs/SQLite/DatabaseSchema/SQLite_TableDescription.swift
@@ -18,6 +18,11 @@ class SQLite_TableDescription {
     let columns: Set<SQLite_ColumnDescription>
     
     
+    /// Creates a new description of a table.
+    ///
+    /// - Parameter name: The table's name.
+    /// - Parameter columns: The table's columns.
+    ///
     init(name: String, columns: Set<SQLite_ColumnDescription>) {
         
         self.name = name
@@ -25,12 +30,26 @@ class SQLite_TableDescription {
     }
     
 
+    /// Returns a column from its name.
+    ///
+    /// - Parameter name: The column's name.
+    ///
+    /// - Returns: The column whose name matches the provided name, or `nil` if
+    ///            the table has no column whose name matches the provided name.
+    ///
     func column(withName name: String) -> SQLite_ColumnDescription? {
         
         return columns.first(where: { $0.name == name })
     }
     
     
+    /// Returns whether the table has a column with a given name.
+    ///
+    /// - Parameter name: The column's name.
+    ///
+    /// - Returns: `true` if the table has a column whose name matches the
+    ///            provided name, `false` otherwise.
+    ///
     func hasColumn(withName name: String) -> Bool {
         
         return column(withName: name) != nil
