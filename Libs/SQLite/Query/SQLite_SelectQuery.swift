56607a03306e062ed20743e3206ba3e0bb4561e9
Alexandre Bintz <alexandre@bintz.io>
2019-02-05 21:02:59 +0100

    wip

--- Libs/SQLite/Query/SQLite_SelectQuery.swift
+++ Libs/SQLite/Query/SQLite_SelectQuery.swift
@@ -3,21 +3,22 @@ import Foundation
 
 
 
-/// A SQL query that selects data from a SQLite database table.
+/// A SQL query that selects data from a table in a SQLite database.
 ///
 /// This type represents queries of the form `SELECT * FROM <table>;`.
 ///
 struct SQLite_SelectQuery: SQLite_Query {
     
     
-    /// A description of the table the query selects data from.
+    /// A description of the table the query should select data from.
     ///
     let tableDescription: SQLite_TableDescription
     
     
     /// Creates a new query.
     ///
-    /// - Parameter tableDescription: The table the query selects data from.
+    /// - Parameter tableDescription: The table the query should select data
+    ///             from.
     ///
     init(selectingFromTable tableDescription: SQLite_TableDescription) {
         
