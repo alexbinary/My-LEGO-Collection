f5cbee1ea13ba14222303ad5cafd5ccbefa9390d
Alexandre Bintz <alexandre@bintz.io>
2019-02-07 19:58:13 +0100

    wip

--- LEGODatabase/InsertStatements/LEGODatabase_ColorInsertStatement.swift
+++ LEGODatabase/InsertStatements/LEGODatabase_ColorInsertStatement.swift
@@ -3,15 +3,31 @@ import Foundation
 
 
 
+/// A statement that inserts LEGO colors into the SQLite database used in the
+/// app to store official LEGO related data.
+///
+/// This class provides high-level methods that are aware of the database
+/// structure.
+///
 class LEGODatabase_ColorInsertStatement: SQLite_InsertStatement {
     
     
+    /// Creates a new statement.
+    ///
+    /// - Parameter connection: The connection to use to compile the query.
+    ///
     init(connection: SQLite_Connection) {
         
         super.init(insertingIntoTable: LEGODatabase.schema.colorsTableDescription, connection: connection)
     }
     
     
+    /// Inserts a color.
+    ///
+    /// - Parameter name: The color's name.
+    /// - Parameter rgb: The colors's RGB representation.
+    /// - Parameter transparent: Whether the colors is transparent.
+    ///
     func insert(name: String, rgb: String, transparent: Bool) {
         
         let table = LEGODatabase.schema.colorsTableDescription
--- LEGODatabase/InsertStatements/LEGODatabase_PartInsertStatement.swift
+++ LEGODatabase/InsertStatements/LEGODatabase_PartInsertStatement.swift
@@ -3,15 +3,30 @@ import Foundation
 
 
 
+/// A statement that inserts LEGO parts into the SQLite database used in the app
+/// to store official LEGO related data.
+///
+/// This class provides high-level methods that are aware of the database
+/// structure.
+///
 class LEGODatabase_PartInsertStatement: SQLite_InsertStatement {
 
     
+    /// Creates a new statement.
+    ///
+    /// - Parameter connection: The connection to use to compile the query.
+    ///
     init(connection: SQLite_Connection) {
         
         super.init(insertingIntoTable: LEGODatabase.schema.partsTableDescription, connection: connection)
     }
     
     
+    /// Inserts a part.
+    ///
+    /// - Parameter name: The part's name.
+    /// - Parameter imageURL: The URL of an image that represents the part.
+    ///
     func insert(name: String, imageURL: String?) {
         
         let table = LEGODatabase.schema.partsTableDescription
