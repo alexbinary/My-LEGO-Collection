2f457d9ca8fc6dba37ff001373d5e9e8a6b35746
Alexandre Bintz <alexandre@bintz.io>
2019-02-07 20:03:43 +0100

    wip

--- Database Tool/Src/Database/DatabaseBuilder.swift
+++ Database Tool/Src/Database/DatabaseBuilder.swift
@@ -3,16 +3,28 @@ import Foundation
 
 
 
+/// A utility class that helps you create and initialize a SQLite database to
+/// use as the database used in the app to store official LEGO related data.
+///
 struct DatabaseBuilder {
     
-    
+   
+    /// Create a SQLite database and creates tables so that it can be used as
+    /// the database used in the app to store official LEGO related data.
+    ///
+    /// - Parameter url: A URL pointing to the location where the database
+    ///             should be created.
+    ///
+    /// - Returns: An instance of `DatabaseInflator` that you can use to insert
+    ///            data into the database that was created.
+    ///
     static func buildDatabase(at url: URL) -> DatabaseInflator {
         
-        print("[DatabaseController] Preparing database at \(url.path)")
+        print("[DatabaseBuilder] Building database at \(url.path)")
         
         guard !FileManager.default.fileExists(atPath: url.path) else {
 
-            fatalError("[DatabaseController] Cannot create database, file exists: \(url.path)")
+            fatalError("[DatabaseBuilder] Cannot create database, file exists: \(url.path)")
         }
         
         let connection = LEGODatabase_Connection(toDatabaseAt: url)
@@ -20,13 +32,10 @@ struct DatabaseBuilder {
         connection.createColorsTable()
         connection.createPartsTable()
         
-        let colorInsertStatement = connection.prepareColorInsertStatement()
-        let partInsertStatement = connection.preparePartInsertStatement()
-
         return DatabaseInflator(
             
-            colorInsertStatement: colorInsertStatement,
-            partInsertStatement: partInsertStatement
+            colorInsertStatement: connection.prepareColorInsertStatement(),
+            partInsertStatement: connection.preparePartInsertStatement()
         )
     }
 }
